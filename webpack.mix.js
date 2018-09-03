5f287c79e9178a336461acdc519e389b6942c137
Deyan Bozhilov <dodobrat98@gmail.com>
2018-09-03 23:03:44 +0300

    tests with front end webpack file

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,6 @@ const {mix} = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
     .styles([
-        'node_modules/bootstrap/dist/css/bootstrap.css',
+        'node_modules/bootstrap/dist/css/bootstrap.min.css',
         'public/css/app.css'
-    ], 'public/css/all.css')
-    .version();
+    ], 'public/css/all.css');
